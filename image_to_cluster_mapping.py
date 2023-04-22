56b1830f66239df2ac6516750f87af2065e7e3c3
Thomas Thaulow Stöcklin <thaulow@thaulow.co>
2023-04-23 00:33:25 +0200

    Update image_to_cluster_mapping.py (#3)

--- image_to_cluster_mapping.py
+++ image_to_cluster_mapping.py
@@ -1,10 +1,10 @@
 import os
 import ast
 
-image_folder = '/home/ubuntu/trainingfiles/cropped_fastmtcnn/'
+image_folder = '/volumes/1/aligned/'
 stablehash_folder =  '/home/ubuntu/trainingfiles/frgc_centers_converted/'
 mapping_file =  '/home/ubuntu/trainingfiles/frgc_results_cluster_id_to_subjects.txt'
-output_file =  '/home/ubuntu/stablehash-reconstruct/training_data.csv'
+output_file =  '/home/ubuntu/stablehash-reconstruct/training_data-100percent.csv'
 
 cluster_list = []
 
@@ -29,7 +29,7 @@ for path in os.scandir(image_folder):
     if path.is_file():
         for cluster_data in cluster_list:
             for image_id in cluster_data[1]:
-                if image_id in path.name:
+                if image_id in path.name: # and int(cluster_data[0]) < 44:
                     o.write(image_folder+path.name+";"+stablehash_folder+'frgc_cluster_'+cluster_data[0]+".npy\n")
                     
 print("Output file generated")
